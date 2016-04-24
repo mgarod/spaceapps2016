6e781fcd09caee0a0f0344457cf718fa7e01a6c5
Abdullah Khan <khanabdullahj@gmail.com>
2016-04-23 20:48:39 -0400

    Returns a tuple (float, string)

--- python/air_check.py
+++ python/air_check.py
@@ -6,24 +6,21 @@ import mraa
 
 get_sensor_value = True
 air_sensor_value = 0
-air_quality_sensor = mraa.Aio(0)
+air_quality_sensor = mraa.Aio(1)
 
-def main():
-    while get_sensor_value:
-        try:
-            air_sensor_value = air_quality_sensor.readFloat()
-            if air_sensor_value > 700:
-                print "High pollution!"
-            elif air_sensor_value > 300:
-                print "Low pollution"
-            else:
-                print "Air is clean enough."
-
-            print "Sensor value: ", air_sensor_value
-            time.sleep(.5)
-        except KeyboardInterrupt:
-            break
-        except IOError:
-            print "Can\'t read sensor."
+def get_air_quality():
+    '''Returns a tuple, a float value and a string.'''
+    try:
+        air_sensor_value = air_quality_sensor.readFloat()
+        pollution = ''
+        if air_sensor_value > 700:
+            pollution = "High pollution!""
+        elif air_sensor_value > 300:
+            pollution = "Low pollution."
+        else:
+            pollution = "Air is clean enough."
+        return air_sensor_value, pollution
+    except IOError:
+        print "Can\'t read sensor."
 
 main()
