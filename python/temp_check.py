ad9d560bde211738507e130e8413a7e019da6fbc
gchai <g@bechai.com>
2016-04-24 13:18:56 -0400

    temp fix

--- python/temp_check.py
+++ python/temp_check.py
@@ -3,12 +3,16 @@ import math
 
 # Connected Grove temp sensor to analog port A2
 
+B=3975
+
 def get_temp():
     try:
         temp_sensor = mraa.Aio(2)
-        t = temp_sensor.readFloat()
-        resistance = (1023 - t) * 10000.0 / t
-        temp = 1 / (math.log(resistance / 10000.0) / t + 1 / 298.15) - 273.15
+        #t = temp_sensor.readFloat()
+        #resistance = (1023 - t) * 10000.0 / t
+        #temp = 1 / (math.log(resistance / 10000.0) / B + 1 / 298.15) - 273.15
+        temp=temp_sensor*.5;
+        temp/=1024.0;
         return temp
     except IOError:
         return
